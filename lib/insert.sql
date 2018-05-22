03f936c94b94d78280bcfa94fc3f195d972a0d98
Megan Dotto <megan.dotto@gmail.com>
2018-05-22 15:49:58 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO series VALUES (1, "A Song of Ice and Fire", 1, 1);
+INSERT INTO series VALUES (2, "Second Series", 2, 2);
+INSERT INTO books VALUES (1, "Game of Thrones", 1996, 1);
+INSERT INTO books VALUES (2, "A Clash of Kings", 1998, 1);
+INSERT INTO books VALUES (3, "A Storm of Swords", 2000, 1);
+INSERT INTO books VALUES (4, "First Book", 2002, 2); 
+INSERT INTO books VALUES (5, "Second Book", 2003, 2);
+INSERT INTO books VALUES (6, "Third Book", 2005, 2);
+INSERT INTO characters VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1);
+INSERT INTO characters VALUES (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1);
+INSERT INTO characters VALUES (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1);
+INSERT INTO characters VALUES (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+INSERT INTO characters VALUES (5, "Character One", "motto one", "cylon", 2, 2);
+INSERT INTO characters VALUES (6, "Character Two", "motto two", "human", 2, 2);
+INSERT INTO characters VALUES (7, "Character Three", "motto three", "cylon", 2, 2);
+INSERT INTO characters VALUES (8, "Character Four", "motto four", "cylon", 2, 2);
+INSERT INTO subgenres VALUES (1, "medieval");
+INSERT INTO subgenres VALUES (2, "space opera");
+INSERT INTO authors VALUES (1, "George R. R. Martin");
+INSERT INTO authors VALUES (2, "Second Author");
+
+
